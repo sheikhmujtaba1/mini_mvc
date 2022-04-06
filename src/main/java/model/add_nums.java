6882950c1ddccd80af3c14be4a0103e742adc8a3
sheikhmujtaba1 <93935069+sheikhmujtaba1@users.noreply.github.com>
2022-04-06 08:58:41 -0400

    added model

---
+++ src/main/java/model/add_nums.java
@@ -0,0 +1,26 @@
+package model;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class add_nums
+{
+	DoubleProperty sum = new SimpleDoubleProperty();
+	
+	
+	/**
+	 * @return the sum
+	 */
+	public DoubleProperty getSum()
+	{
+		return sum;
+	}
+
+
+	public add_nums()
+	{
+		
+	}
+	
+	
+}
